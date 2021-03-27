abf825d61a94355f6a3dd412c947eb1b6f0c34b7
Franfercs <ffcs98@hotmail.com>
2021-03-27 19:51:34 -0300

    Arregle algo

--- prueba/src/main/java/py/com/progweb/prueba/ejb/BolsaDAO.java
+++ prueba/src/main/java/py/com/progweb/prueba/ejb/BolsaDAO.java
@@ -1,5 +1,35 @@
 package py.com.progweb.prueba.ejb;
 
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import py.com.progweb.prueba.model.Bolsa;
+
+@Stateless
 public class BolsaDAO {
-    
+    @PersistenceContext(unitName = "pruebaPU")
+    private EntityManager en;
+
+    public void agregar(Bolsa entidad){
+        this.en.persist(entidad);
+    }
+
+    @SuppressWarnings("unchecked") 
+    public List<Bolsa> lista(){
+        Query b = this.en.createQuery("select p from Bolsa p");
+        return (List<Bolsa>) b.getResultList();
+    }
+
+    public void actualizar(int id, Bolsa entidad) {
+        Bolsa b = this.en.merge(entidad);
+    }
+
+    public void eliminar(int id){
+        Bolsa b = this.en.find(Bolsa.class, id);
+        this.en.remove(b);
+    }
 }
