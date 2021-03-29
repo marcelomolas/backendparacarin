af550d0ce458755a575051924cd7d3f346a927ca
Franfercs <ffcs98@hotmail.com>
2021-03-28 22:30:23 -0400

    Ya funciona agregar cliente en el frontend

--- react/frontend/src/components/displayAgregarCliente.js
+++
@@ -1,8 +0,0 @@
-function toggleOverlay() {
-    let overlayDiv = document.querySelector(".overlay");
-    console.log(overlayDiv.style.opacity);
-    if (overlayDiv.style.opacity === '0')
-      overlayDiv.style.opacity = 1;
-    else
-      overlayDiv.style.opacity = 0;
-    }
